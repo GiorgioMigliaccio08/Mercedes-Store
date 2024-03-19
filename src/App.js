1c35abdbf45c6e97d23d82998b565b8a335085b4
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2024-03-19 17:15:42 +0100

    Add Modify to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 import NavBar from "../src/components/NavBar";
 import Banner from "../src/components/Banner";
+import Header from "../src/components/Header";
 
 function App() {
   return (
     <>
       <NavBar />
       <Banner />
+      <Header />
     </>
   );
 }
