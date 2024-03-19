e45f20a50f715213760d640a64a3dc4bc7f0bec5
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2024-03-19 17:16:57 +0100

    Add modify to Bnner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,6 +2,7 @@ import Carousel from "react-bootstrap/Carousel";
 import img1 from "../assets/MercedesA.jpg";
 import img2 from "../assets/MercedesB.jpg";
 import img3 from "../assets/MercedesD.jpg";
+import img4 from "../assets/MercedesC.jpg";
 
 function Banner() {
   return (
@@ -9,22 +10,37 @@ function Banner() {
       <Carousel.Item>
         <img className="d-block w-100" src={img1} alt="First slide" />
         <Carousel.Caption>
-          <h3>Classe A</h3>
-          <p>Scopri tutte le nostre vetture in pronta consegna</p>
+          <h3 className="text">Classe A</h3>
+          <p className="text">
+            Scopri tutte le nostre vetture in pronta consegna
+          </p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img className="d-block w-100" src={img2} alt="Second slide" />
         <Carousel.Caption>
-          <h3>Coupè</h3>
-          <p>Scopri tutte le nostre vetture in pronta consegna</p>
+          <h3 className="text">Coupè</h3>
+          <p className="text">
+            Scopri tutte le nostre vetture in pronta consegna
+          </p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img className="d-block w-100" src={img3} alt="Third slide" />
         <Carousel.Caption>
-          <h3>Berline</h3>
-          <p>Scopri tutte le nostre vetture in pronta consegna</p>
+          <h3 className="text">Berline</h3>
+          <p className="text">
+            Scopri tutte le nostre vetture in pronta consegna
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={img4} alt="Third slide" />
+        <Carousel.Caption>
+          <h3 className="text">Suv & Fuoristrada</h3>
+          <p className="text">
+            Scopri tutte le nostre vetture in pronta consegna
+          </p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
