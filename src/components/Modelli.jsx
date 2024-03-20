b92f9ffe8062d597598f74017f05596a3d2f2171
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2024-03-20 13:00:52 +0100

    Add Modelli.jsx

---
+++ src/components/Modelli.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Modelli = () => {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-6">
+          <div className="card mb-3">
+            <div className="card-body">
+              <h5 className="card-title">Card 1</h5>
+              <p className="card-text">Contenuto della card 1...</p>
+            </div>
+          </div>
+          <div className="card mb-3">
+            <div className="card-body">
+              <h5 className="card-title">Card 2</h5>
+              <p className="card-text">Contenuto della card 2...</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <ul className="list-group">
+            <li className="list-group-item">Elemento 1</li>
+            <li className="list-group-item">Elemento 2</li>
+            <li className="list-group-item">Elemento 3</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modelli;
