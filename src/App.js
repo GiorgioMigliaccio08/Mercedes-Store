335b42a664dcf3c45142deaf44cc196da4358c5c
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2024-03-22 21:20:59 +0100

    Modify App.js

--- src/App.js
+++ src/App.js
@@ -2,23 +2,21 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import NavBar from "../src/components/NavBar";
-import Banner from "../src/components/Banner";
-import Header from "../src/components/Header";
 import Modelli from "../src/components/Modelli";
-import AquistaAuto from "../src/components/AquistaAuto";
+import AcquistaAuto from "../src/components/AquistaAuto";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Banner />
-      <Header />
-      <Routes>
-        <Route element={<Modelli />} path="/modelli" />
-        <Route element={<AquistaAuto />} path="/aquistaauto" />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Home />} path="/" />
+          <Route element={<AcquistaAuto />} path="/aquistaauto" />
+          <Route element={<Modelli />} path="/modelli" />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
