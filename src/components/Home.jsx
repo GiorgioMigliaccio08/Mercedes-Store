114ce475d8661ae20ce02731b357fd4ab510146f
Giorgio Migliaccio <giorgiomigliaccio08@gmail.com>
2024-03-22 21:22:25 +0100

    Add Home

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import NavBar from "./NavBar";
+import Banner from "./Banner";
+import Header from "./Header";
+
+const Home = () => {
+  return (
+    <div>
+      <NavBar />
+      <Banner />
+      <Header />
+    </div>
+  );
+};
+
+export default Home;
